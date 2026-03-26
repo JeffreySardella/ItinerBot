951003746abbc41fb9e8f07561b8c9ec6dbb16db
Jeff <sardellajeffrey@yahoo.com>
2026-03-25 23:00:04 -0700

    Choose certain calendars

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ TRIP_END_DATE = date.fromisoformat(os.environ["TRIP_END_DATE"])
 TIMEZONE = os.environ.get("TIMEZONE", "America/Los_Angeles")
 GOOGLE_CREDENTIALS = os.environ.get("GOOGLE_CREDENTIALS", "credentials.json")
 SERPAPI_KEY = os.environ.get("SERPAPI_KEY", "")
+CALENDAR_ID = os.environ.get("CALENDAR_ID", "primary")
 
 if TRIP_START_DATE >= TRIP_END_DATE:
     raise ValueError("TRIP_START_DATE must be before TRIP_END_DATE")
